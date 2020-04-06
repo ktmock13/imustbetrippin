83259e8cb1e3f9204174d2399b23ca0cb93b2d44
ktmock13 <ktmock13@gmail.com>
2020-04-05 17:41:09 -0700

    remove public from git

--- public/path----fa35807a0b71799c5e20.js
+++ public/path----fa35807a0b71799c5e20.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{102:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Adventure Heads"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{101:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Adventure Heads"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----fa35807a0b71799c5e20.js.map
