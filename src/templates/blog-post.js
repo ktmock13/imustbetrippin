bdf1b73a58575187a9b4c453584119ecb4933d0d
Alyssa neubauer <neubauer.30@osu.edu>
2020-04-08 20:43:52 -0700

    remove unecessary images array

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,20 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import ImageGallery from 'react-image-gallery'
-const images = [
-  {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
-  },
-  {
-    original: 'https://picsum.photos/id/1015/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1015/250/150/',
-  },
-  {
-    original: 'https://picsum.photos/id/1019/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1019/250/150/',
-  },
-]
 
 export default function Template({ data }) {
   const post = data.markdownRemark
