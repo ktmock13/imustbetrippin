4b01ae4c341fcf18e2cfdebc6df95f8c06bb43bd
Alyssa neubauer <neubauer.30@osu.edu>
2020-04-19 11:55:19 -0700

    breakt out menu stlying into separate file

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
-
+import '../styles/menu.css'
 const Menu = () => (
-  <div
-    style={{
-      background: '#f4f4f4',
-      paddingTop: '10px',
-    }}
-  >
-    <ul
-      style={{
-        listStyle: 'none',
-        display: 'flex',
-        justifyContent: 'space-evenly',
-      }}
-    >
+  <div className="menu-wrapper">
+    <ul className="menu-item">
       <li>
         <Link to="/blog">Blog</Link>
       </li>
