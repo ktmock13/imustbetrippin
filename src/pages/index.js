d0afe494695f7d09566e8cd83f376e826637ba2b
Alyssa neubauer <neubauer.30@osu.edu>
2020-04-19 15:12:24 -0700

    redesign home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,17 @@ import '../styles/home.scss'
 
 const IndexPage = props => {
   console.log(props)
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <h1 className="adventure-heads-title">Welcome to Adventure Heads!</h1>
+      <div className="adventure-heads-subtitle">
+        this is the home of all of Keith and Alyssa's random adventures. This
+        site consists of mostly Moto trips, motor bike upgrades, and info. We
+        also plan to add various hiking adventures and more to the site as well.
+        Stay Tuned!
+      </div>
+    </Layout>
+  )
 }
 
 export default IndexPage
