7b4864ef4c545e2e0fa1b18e433b95f74b4d7065
Alyssa neubauer <neubauer.30@osu.edu>
2020-04-19 17:05:36 -0700

    add backgrounf to blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,38 +2,41 @@ import React from 'react'
 import Link from 'gatsby-link'
 import ImageGallery from 'react-image-gallery'
 import Layout from '../components/layout'
+import SubpageLayout from '../components/subpage'
 
 export default function Template({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
-      <Link to="/blog">Go Back</Link>
-      <hr />
-      <h1>{post.frontmatter.title}</h1>
-      <h4>
-        Posted by {post.frontmatter.author} on {post.frontmatter.datePosted}
-      </h4>
-      <div className="image-gallery">
-        <ImageGallery
-          items={post.frontmatter.photos.map(photo => ({
-            original: photo,
-            thumbnail: photo,
-          }))}
-          showPlayButton={false}
-          showBullets={true}
+      <SubpageLayout>
+        <Link to="/blog">Go Back</Link>
+        <hr />
+        <h1>{post.frontmatter.title}</h1>
+        <h4>
+          Posted by {post.frontmatter.author} on {post.frontmatter.datePosted}
+        </h4>
+        <div className="image-gallery">
+          <ImageGallery
+            items={post.frontmatter.photos.map(photo => ({
+              original: photo,
+              thumbnail: photo,
+            }))}
+            showPlayButton={false}
+            showBullets={true}
+          />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="${post.frontmatter.map}" width="640" height="480"></iframe>`,
+          }}
         />
-      </div>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="${post.frontmatter.map}" width="640" height="480"></iframe>`,
-        }}
-      />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `<iframe width="560" height="315" src="${post.frontmatter.video}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
-        }}
-      />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `<iframe width="560" height="315" src="${post.frontmatter.video}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+          }}
+        />
+      </SubpageLayout>
     </Layout>
   )
 }
