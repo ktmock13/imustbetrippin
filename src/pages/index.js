d480dd166c8dcfb3d90c0a4a37841d1751705671
Keith Mock <kmock@apple.com>
2020-12-29 13:03:32 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,39 @@
 import { Link } from 'gatsby';
-import React from 'react'
+import React, { useState } from 'react';
 import classes from '../styles/index.module.css';
 // import shatsaVid from '../pages.shasta.MP4';
 
 const IndexPage = () => {
+  const [videoLoaded, setVideoLoaded] =  useState(false);
   const videoSource = 
     navigator.userAgent.indexOf('Chrome')+1 ? 
     "https://adventure-head-photos.s3-us-west-2.amazonaws.com/videos/splash.webm" : 
-    "https://adventure-head-photos.s3-us-west-2.amazonaws.com/videos/shasta.MP4"
+    "https://adventure-head-photos.s3-us-west-2.amazonaws.com/videos/splash.mp4"
   return (
       <div className={classes.Container} >
-          <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
+          <video 
+            autoPlay="autoplay" 
+            loop="loop" 
+            muted 
+            className={`${classes.Video} ${videoLoaded ? classes.VideoFinish: ''}`} 
+            onLoadStart={() => {}}
+            onCanPlay={() => { 
+              console.log('vid done')
+              setVideoLoaded(true)
+            }}
+          >
               <source src={videoSource} type="video/mp4" />
               Your browser does not support the video tag.
           </video>
 
           <div className={classes.Content}>
-              <div className={classes.SubContent} >
+              <div className={`${classes.SubContent} ${videoLoaded ? classes.ShowOnFinish: ''}` } >
                   <h1 className={classes.Title}>I must be trippin.</h1>
                   <Link className={classes.Link}to={'/trips'}> <span> go to trips </span></Link>
+              </div>
+              <div className={`${classes.RainbowSubContent} ${videoLoaded ? classes.HideOnFinish: ''}` } >
+                  <h1 className={classes.RainbowTitle}>I must be trippin.</h1>
+                  <Link className={classes.RainbowLink}to={'/trips'}> <span> go to trips </span></Link>
               </div>
           </div>
       </div>
