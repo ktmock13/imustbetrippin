0bb8dcbcd0e85ac4e2e24ea7761b92a1030ad5cc
Keith Mock <kmock@apple.com>
2020-12-28 20:31:03 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,21 @@
+import { Link } from 'gatsby';
 import React from 'react'
 import classes from '../styles/index.module.css';
-import shatsaVid from '../pages/shasta.MP4';
+// import shatsaVid from '../pages.shasta.MP4';
 
 const IndexPage = () => {
   const videoSource = "https://www.w3schools.com/tags/movie.mp4"
   return (
       <div className={classes.Container} >
           <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
-              <source src={shatsaVid} type="video/mp4" />
+              <source src={videoSource} type="video/mp4" />
               Your browser does not support the video tag.
           </video>
 
           <div className={classes.Content}>
               <div className={classes.SubContent} >
-                  <h1>Reactjs Course</h1>
-                  <p>Learn how to develope React projects</p>
-                  <button type="button" className="btn btn-outline-dark">View the course</button>
-                  <img
-                      src="https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-                      alt="profile" />
+                  <h1 className={classes.Title}>I must be trippin.</h1>
+                  <Link className={classes.Link}to={'/trips'}> <span class='link'>go to trips </span></Link>
               </div>
           </div>
       </div>
