45280c2e88f7510e3a3c09067a14fe3458b1a832
Keith Mock <kmock@apple.com>
2020-12-28 20:02:09 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import Link from 'gatsby-link'
-import Layout from '../components/layout'
+import classes from '../styles/index.module.css';
+import shatsaVid from '../pages/shasta.MP4';
 
-const IndexPage = (props) => {
-  console.log(props)
+const IndexPage = () => {
+  const videoSource = "https://www.w3schools.com/tags/movie.mp4"
   return (
-    <Layout hasMenu={false}>
-      <h1>Welcome to Adventure Heads</h1>
-      <p>This is a site to show all of my adventures</p>
-      <Link to="/trips">Check out the Blog page!</Link>
-    </Layout>
+      <div className={classes.Container} >
+          <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
+              <source src={shatsaVid} type="video/mp4" />
+              Your browser does not support the video tag.
+          </video>
+
+          <div className={classes.Content}>
+              <div className={classes.SubContent} >
+                  <h1>Reactjs Course</h1>
+                  <p>Learn how to develope React projects</p>
+                  <button type="button" className="btn btn-outline-dark">View the course</button>
+                  <img
+                      src="https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                      alt="profile" />
+              </div>
+          </div>
+      </div>
   )
 }
 
