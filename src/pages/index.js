652268a0ddd2314b0f89ae2a5bab3531e824fc2e
Keith Mock <kmock@apple.com>
2020-12-29 11:25:12 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,10 @@ import classes from '../styles/index.module.css';
 // import shatsaVid from '../pages.shasta.MP4';
 
 const IndexPage = () => {
-  const videoSource = "https://adventure-head-photos.s3-us-west-2.amazonaws.com/videos/splash.webm"
+  const videoSource = 
+    navigator.userAgent.indexOf('Chrome')+1 ? 
+    "https://adventure-head-photos.s3-us-west-2.amazonaws.com/videos/splash.webm" : 
+    "https://adventure-head-photos.s3-us-west-2.amazonaws.com/videos/shasta.MP4"
   return (
       <div className={classes.Container} >
           <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
@@ -15,7 +18,7 @@ const IndexPage = () => {
           <div className={classes.Content}>
               <div className={classes.SubContent} >
                   <h1 className={classes.Title}>I must be trippin.</h1>
-                  <Link className={classes.Link}to={'/trips'}> <span class='link'>go to trips </span></Link>
+                  <Link className={classes.Link}to={'/trips'}> <span> go to trips </span></Link>
               </div>
           </div>
       </div>
