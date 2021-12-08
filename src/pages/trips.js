a5cb0a062f51d9ee5173f677127913d1c0d3206c
Keith Mock <kmock@apple.com>
2021-12-07 23:23:43 -0800

    mobile

--- src/pages/trips.js
+++ src/pages/trips.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 import TagList from '../components/tag-list'
 import TagFilter from '../components/TagFilter'
 import Layout from '../components/layout'
-import classes from '../styles/index.module.css'
+import '../styles/trips.css'
 import statemap from '../state_map.json'
 
 const BlogPage = ({ data }) => {
@@ -16,17 +16,25 @@ const BlogPage = ({ data }) => {
   return (
     <Layout>
       <div
-        className={`{classes.map}`}
+        className="fullMap"
         dangerouslySetInnerHTML={{
           __html: `<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1UfcHSqhZQY-YKoYASi-E1tr_9fu_fLfQ&ehbc=2E312F" width="1080px" height="375"></iframe>`,
         }}
       />
+      <div className="maplegend">
+        <span className="legenditem">
+          <span className="maplegendvan"></span> van
+        </span>
+        <span className="legenditem">
+          <span className="maplegendmoto"></span> moto
+        </span>
+      </div>
       <TagFilter posts={posts} />
       {posts.map((post) => (
         <Link to={post.node.frontmatter.path}>
           <div key={post.node.id}>
             <h3>{post.node.frontmatter.title}</h3>
-            <h3 className={classes.stateface}>
+            <h3 className="stateface">
               {post.node.frontmatter.states &&
                 post.node.frontmatter.states.map((state) => (
                   <span>{statemap[state]}</span>
